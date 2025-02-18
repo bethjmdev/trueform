7dd0270c25d96b4beddc36a05a6f52232d84d52f
bethjm <b.jeanne.mills@gmail.com>
2025-02-18 12:39:10 -0700

    added navbar styling

--- src/components/clientview/ClientHomePage.jsx
+++ src/components/clientview/ClientHomePage.jsx
@@ -3,27 +3,35 @@ import { signOut } from "firebase/auth";
 import { useAuth } from "../../utils/auth/AuthProvider";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 
+import "./ClientHomePage.css";
+
 const ClientHomePage = () => {
   const user = useAuth();
   const navigate = useNavigate();
 
   return (
-    <div>
-      <p>hi Client</p>
-      <button onClick={() => signOut(auth)}>Sign Out</button>
-      <br />
-      <br />
-      <button onClick={() => navigate("/view-workout")}>View Workouts</button>
-      <br />
-      <br />
-      <button onClick={() => navigate("/client-view-profile")}>
-        View Profile
-      </button>
-      <br />
-      <br />
-      <button onClick={() => navigate("/client-past-workouts")}>
-        View Past Workouts
-      </button>
+    <div className="ClientHomePage">
+      <div className="client_homepage_container">
+        <p>hi Client</p>
+        <div className="homepage_buttons">
+          <button onClick={() => signOut(auth)}>Sign Out</button>
+          <br />
+          <br />
+          <button onClick={() => navigate("/view-workout")}>
+            View Workouts
+          </button>
+          <br />
+          <br />
+          <button onClick={() => navigate("/client-view-profile")}>
+            View Profile
+          </button>
+          <br />
+          <br />
+          <button onClick={() => navigate("/client-past-workouts")}>
+            View Past Workouts
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
